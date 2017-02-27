0ae83f38793937e483c55583606c8e363f820b72
Marco Reichenberger <IPSW1@users.noreply.github.com>
2017-02-27 15:53:30 +0100

    Make web interface compatible with data format

--- raspberry_pi/www/index.php
+++ raspberry_pi/www/index.php
@@ -9,38 +9,21 @@
 
 <body>
 <?php
-	//MySQL userdata
-	$username = "";
-	$password = "";
-	$hostname = "";
-	$db = "";
-	$db_table = "";
-
-	//Variables that should match the SQL columns
-	$temp = "temp";
-	$hum = "hum";
-	$press = "press";
-	$forec = "forecast";
-	$datetime = "dtime";
-
-	//Connect to MySQL and select database
-	$connection = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
-	mysql_select_db($db) or die("Database does not exist");
-
+	// read weather data and time from textfile
 	header('Content-Type: text/html; charset=UTF-8');
-
-	//Daten aus Datenbank fuer aktuelle Daten abfragen
-	$query = "SELECT * FROM ".$db_table." ORDER BY ".$datetime." DESC LIMIT 1";
-	$return = mysql_query($query);
-	$data = mysql_fetch_array($return);
+	$weather_data_file = fopen("/var/www/html/current_data.txt", "r") or die("Unable to open file!");
+	$weather_data_str = fread($weather_data_file, filesize("/var/www/html/current_data.txt"));
+	$weather_data_arr = split(";", $weather_data_str);
+	fclose($weather_data_file);
 ?>
+
 <br></br>
 	<!-- HTML table to display weatherdata -->
 	<table align="center">
 		<tr>
-			<td align="center" width=280><div class = "data"><?php echo round($data[$temp]);?></div></td>
-			<td align="center" width=280><div class = "data"><?php echo round($data[$hum]);?></div></td>
-			<td align="center" width=280><div class = "data"><?php echo round($data[$press]);?></div></td>
+			<td align="center" width=280><div class = "data"><?php echo $weather_data_arr[0];?></div></td>
+			<td align="center" width=280><div class = "data"><?php echo $weather_data_arr[1];?></div></td>
+			<td align="center" width=280><div class = "data"><?php echo $weather_data_arr[2];?></div></td>
 		</tr>
 		<tr>
 			<td align="center" height=1><table align="center"><tr><td align="center" height=1 width=200 style="border-bottom: 1px solid #aaaaaa"></td></tr></table></td>
@@ -55,18 +38,17 @@
 	</table>
 
 <br></br>
-<br></br>
 
-
-<!-- Forecast -->
-<div class = "forecast">Forecast: <?php echo $data[$forec];?></div>
+<!-- display forecast -->
+<div class = "forecast">Forecast: <?php echo $weather_data_arr[3];?></div>
 <br></br>
 <br></br>
 
+<!-- style switcher and display of time of last update -->
 <div align='center'>
 		<a href="#" onclick="setActiveStyleSheet('default'); return false;">Dark</a>&nbsp &nbsp <a href="#" onclick="setActiveStyleSheet('alternate'); return false;">Light</a>
 		<p></p>
-		<div class = "label">Last update: <?php echo $data[$datetime];?></div>
+		<div class = "label">Last update: <?php echo $weather_data_arr[4];?></div>
 </div>
 
 </body>
